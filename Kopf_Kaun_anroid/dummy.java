5779d7b76ea2215eac80b94c64f8c0ed9b61aeac
fabi_2k03 <fabian.kopf03@gmail.com>
2025-05-20 13:58:09 +0200

    Solved some code errors

--- Kopf_Kaun_anroid/dummy.java
+++ Kopf_Kaun_anroid/dummy.java
@@ -1,35 +1,24 @@
-<<<<<<< HEAD
-public void thisIsANewMethod() { 
-	int ctr = 0;
-	for (int i = 0; i < 10; i++) {
-		ctr++;
-	}
-	System.out.println("Counter: " + ctr);
+public class dummy {
 
-	System.out.println("Fixed the bug!")
-}
-public void thisIsANewMethod() { 
-	System.out.println("Hello World!");
-}
-
-public class SimpleCalculation {
-
-    public static void main(String[] args) {
-        int a = 6;
-        int b = 7;
-        int result = a * b;
-
-        System.out.println("Das Ergebnis von " + a + " * " + b + " ist: " + result);
-    }
-}
-=======
-/* HelloWorld.java
- */
-
-public class HelloWorld
-{
 	public static void main(String[] args) {
+		int a = 6;
+		int b = 7;
+		int result = a * b;
+
+		System.out.println("Das Ergebnis von " + a + " * " + b + " ist: " + result);
+	}
+
+	public void thisIsANewMethod() {
+		int ctr = 0;
+		for (int i = 0; i < 10; i++) {
+			ctr++;
+		}
+		System.out.println("Counter: " + ctr);
+
+		System.out.println("Fixed the bug!");
+	}
+
+	public void thisIsANewMethod2() {
 		System.out.println("Hello World!");
 	}
 }
->>>>>>> a3ea717f31a6bef43850538928947ecd61d8c831
