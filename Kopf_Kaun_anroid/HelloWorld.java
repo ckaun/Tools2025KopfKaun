32585d15300fd1061768b4df79f61d5ef7ce759c
s2410824008 <s2410824008@students.fh-hagenberg.at>
2025-05-23 12:40:04 +0200

    changed HelloWorld.java

--- Kopf_Kaun_anroid/HelloWorld.java
+++ Kopf_Kaun_anroid/HelloWorld.java
@@ -1,9 +1,28 @@
-/*
- * HelloWorld.java
- */
-
-public class HelloWorld {
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        String word = "racecar";
+        if (isPalindrome(word)) {
+            System.out.println(word + " is a palindrome.");
+        } else {
+            System.out.println(word + " is not a palindrome.");
+        }
+    }
+
+    public static boolean isPalindrome(String str) {
+        // Remove spaces and convert to lowercase for a case-insensitive comparison
+        str = str.replaceAll("\\s+", "").toLowerCase();
+
+        int left = 0;
+        int right = str.length() - 1;
+
+        while (left < right) {
+            if (str.charAt(left) != str.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+
+        return true;
     }
 }
